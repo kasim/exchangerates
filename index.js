0338a947f1259f1e53a081689b263ddc02fed9ce
Steven Chu <chu.chongmeng@gmail.com>
2017-08-10 12:50:12 +0800

    Streamlined code in index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express'),
-    oxr = require('open-exchange-rates'),
     nodeCache = require('node-cache'),
     dateFormat = require('dateformat'),
-    Promise = require('promise');
+    Promise = require('promise'),
+    app = express(),
+    router = express.Router(),
+    fxCache = new nodeCache();
 
-var fx = require('money');
-const app = express();
-const router = express.Router();
-const fxCache = new nodeCache();
+var oxr = require('open-exchange-rates'),
+    fx = require('money');
 
 oxr.set({app_id: '2a3ffbc790a9435b94d3f219fac0a98e'});
 
@@ -37,14 +37,6 @@ var historical = function(date) {
     });    
 }
 
-//function setHistorical(date){
-//    historical(date).then(setCache(date));
-//}
-
-//function setLatest(date){
-//    latest.then(setCache(date));
-//}
-
 function getRates(date){
     if(date === dateFormat(new Date(), 'yyyy-mm-dd')) latest.then(setCache(date));
     else  historical(date).then(setCache(date));
@@ -58,39 +50,38 @@ function setCache(date){
 
 function getCache(date){
     var localCache = fxCache.get(date);
-    if (localCache == undefined) setRates(date);
+    if (localCache == undefined) getRates(date);
     else fx = localCache;
 }
 
-
 router.get('/latest/all', function(req, res) {
     getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
-    res.json({base: fx.base, rates:fx.rates, timestamp:dateFormat(new Date(), 'yyyy-mm-dd')})
+    res.json({base: fx.base, rates:fx.rates, date:dateFormat(new Date(), 'yyyy-mm-dd')});
 });
 
 router.get('/latest/:curr', function(req, res) {
     getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
-    res.json({base: fx.base, rates:fx.rates[req.params.curr], timestamp:dateFormat(new Date(), 'yyyy-mm-dd')})
+    res.json({base: fx.base, rates:fx.rates[req.params.curr], date:dateFormat(new Date(), 'yyyy-mm-dd')});
 });
 
 router.get('/latest/:curr/base/:base', function(req, res) {
     getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
-    res.json({base: req.params.base, rates:fx(1).from(req.params.base).to(req.params.curr), timestamp:dateFormat(new Date(), 'yyyy-mm-dd')})
+    res.json({base: req.params.base, rates:fx(1).from(req.params.base).to(req.params.curr), date:dateFormat(new Date(), 'yyyy-mm-dd')});
 });
 
 router.get('/historical/:date/all', function(req, res) {
     getCache(req.params.date);
-    res.json({base: fx.base, rates:fx.rates, timestamp:req.params.date})
+    res.json({base: fx.base, rates:fx.rates, date:req.params.date});
 });
 
 router.get('/historical/:date/:curr', function(req, res) {
     getCache(req.params.date);
-    res.json({base: fx.base, rates:fx.rates[req.params.curr], timestamp:req.params.date})
+    res.json({base: fx.base, rates:fx.rates[req.params.curr], date:req.params.date});
 });
 
-router.get('/latest/:curr/base/:base', function(req, res) {
-    getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
-    res.json({base: req.params.base, rates:fx(1).from(req.params.base).to(req.params.curr), timestamp:dateFormat(new Date(), 'yyyy-mm-dd')})
+router.get('/historical/:date/:curr/base/:base', function(req, res) {
+    getCache(req.params.date);
+    res.json({base: req.params.base, rates:fx(1).from(req.params.base).to(req.params.curr), date:req.params.date});
 });
 
 app.use('/api', router);
