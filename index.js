c0d0a102beba4719c628bc80eff2528968eeab56
Steven Chu <chu.chongmeng@gmail.com>
2017-08-11 01:36:59 +0800

    Refactoring the code to get exchange rates from lib writen by me

--- index.js
+++ index.js
@@ -1,91 +1,38 @@
 const express = require('express'),
-    nodeCache = require('node-cache'),
     dateFormat = require('dateformat'),
-    Promise = require('promise'),
     app = express(),
-    router = express.Router(),
-    fxCache = new nodeCache();
+    router = express.Router();
 
-var oxr = require('open-exchange-rates'),
-    fx = require('money');
-
-oxr.set({app_id: '2a3ffbc790a9435b94d3f219fac0a98e'});
-
-var latest = new Promise(function (resolve, reject) {
-    oxr.latest(function(err, res) {
-	if (err) reject(err);
-	else { 
-	    fx.base = oxr.base;
-	    fx.rates = oxr.rates;
-	    fx.timestamp = oxr.timestamp;
-	    resolve(res);
-	}
-    });
-});
-
-var historical = function(date) {
-    return new Promise(function(resolve, reject) {
-	oxr.historical(date, function(err, res) {
-	    if (err) reject(err);
-	    else {
-		fx.base = oxr.base;
-		fx.rates = oxr.rates;
-		fx.timestamp = oxr.timestamp;
-		resolve(res);
-	    }
-	});
-    });    
-}
-
-function getRates(date){
-    if(date === dateFormat(new Date(), 'yyyy-mm-dd')) latest.then(setCache(date));
-    else  historical(date).then(setCache(date));
-}
-
-function setCache(date){
-    fxCache.set(date, fx.rates, function( err, success ){
-	if( !err && success ) console.log( "Date : " + date + " save in cache!" );
-    });
-}
-
-function getCache(date){
-    var cacheRates = fxCache.get(date);
-    if (cacheRates == undefined) getRates(date);
-    else { 
-	fx.rates = cacheRates;
-	fx.timestamp = date;
-	fx.base = "USD";
-    }
-}
+var grates = require('./lib/get-rates.js');
 
 router.get('/latest/all', function(req, res) {
-    getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
-    res.json({base: fx.base, rates:fx.rates, date:dateFormat(new Date(), 'yyyy-mm-dd')});
+    grates.getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
+    res.json({base: grates.fx.base, rates:grates.fx.rates, date:dateFormat(new Date(), 'yyyy-mm-dd')});
 });
 
 router.get('/latest/:curr', function(req, res) {
-    getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
-    res.json({base: fx.base, rates:fx.rates[req.params.curr], date:dateFormat(new Date(), 'yyyy-mm-dd')});
+    grates.getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
+    res.json({base: grates.fx.base, rates:grates.fx.rates[req.params.curr], date:dateFormat(new Date(), 'yyyy-mm-dd')});
 });
 
 router.get('/latest/:curr/base/:base', function(req, res) {
-    getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
-    res.json({base: req.params.base, rates:fx(1).from(req.params.base).to(req.params.curr), date:dateFormat(new Date(), 'yyyy-mm-dd')});
+    grates.getCache(dateFormat(new Date(), 'yyyy-mm-dd'));
+    res.json({base: req.params.base, rates:grates.fx(1).from(req.params.base).to(req.params.curr), date:dateFormat(new Date(), 'yyyy-mm-dd')});
 });
 
 router.get('/historical/:date/all', function(req, res) {
-    getCache(req.params.date);
-    res.json({base: fx.base, rates:fx.rates, date:req.params.date});
+    grates.getCache(req.params.date);
+    res.json({base: grates.fx.base, rates:grates.fx.rates, date:req.params.date});
 });
 
 router.get('/historical/:date/:curr', function(req, res) {
-    getCache(req.params.date);
-    res.json({base: fx.base, rates:fx.rates[req.params.curr], date:req.params.date});
+    grates.getCache(req.params.date);
+    res.json({base: grates.fx.base, rates:grates.fx.rates[req.params.curr], date:req.params.date});
 });
 
 router.get('/historical/:date/:curr/base/:base', function(req, res) {
-    getCache(req.params.date);
-    res.json({base: req.params.base, rates:fx(1).from(req.params.base).to(req.params.curr), date:req.params.date});
+    grates.getCache(req.params.date);
+    res.json({base: req.params.base, rates:grates.fx(1).from(req.params.base).to(req.params.curr), date:req.params.date});
 });
 
 app.use('/api', router);
